b8a122a9920f299fe2186d24b77603098252d2e3
张学强 <zhang.xueqiang>
2023-02-02 15:40:11 +0800

    feat: 优化代码，添加列表引擎页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,35 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-Vue.use(VueRouter)
-export default  new VueRouter({
-    mode: 'hash',
-    routes:   [
-         // 没有权限的页面
-         {
-           path: '/test',
-           component: () =>
-             import('@/utils/commonRender/example/tableExample'),
-           name: 'test',
-         },
-         {
-           path: '/test1',
-           component: () =>
-             import('@/utils/commonRender/example/tableExample2'),
-           name: 'test1',
-         },
-         {
-           path: '/test2',
-           component: () =>
-             import('@/utils/commonRender/example/formExample'),
-           name: 'test2',
-         },
-       ]
-  })
-
-
-
-
-
+Vue.use(VueRouter);
+export default new VueRouter({
+  mode: 'hash',
+  routes: [
+    {
+      path: '/',
+      redirect: 'listEngine',
+    },
+    {
+      path: '/test',
+      component: () =>
+        import('@/CommonDP/utils/commonRender/example/tableExample'),
+      name: 'test',
+    },
+    {
+      path: '/test1',
+      component: () =>
+        import('@/CommonDP/utils/commonRender/example/tableExample2'),
+      name: 'test1',
+    },
+    {
+      path: '/test2',
+      component: () =>
+        import('@/CommonDP/utils/commonRender/example/formExample'),
+      name: 'test2',
+    },
+    {
+      path: '/listEngine',
+      component: () => import('@/CommonDP/lowCode/listEngine'),
+      name: 'listEngine',
+    },
+  ],
+});
