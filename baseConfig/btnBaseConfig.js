14371971df8b079868321ed97e7ce59f98e59c9e
张学强 <zhang.xueqiang>
2023-02-22 18:11:15 +0800

    feat: 添加按钮配置页面

---
+++ baseConfig/btnBaseConfig.js
@@ -0,0 +1,325 @@
+export const openType = [
+  {
+    id: 0,
+    cnName: '当前页面打开',
+  },
+  {
+    id: 1,
+    cnName: '新窗口打开',
+  },
+];
+
+export const showType = [
+  {
+    value: 0,
+    label: '纯文字',
+  },
+  {
+    value: 1,
+    label: '纯图标',
+  },
+  {
+    value: 2,
+    label: '图标+文字',
+  },
+];
+
+export const size = [
+  {
+    value: '',
+    label: '默认',
+  },
+  {
+    value: 'medium',
+    label: '中等',
+  },
+  {
+    label: '小型',
+    value: 'small',
+  },
+  {
+    label: '超小',
+    value: 'mini',
+  },
+];
+
+export const yesOrNo = [
+  {
+    value: true,
+    label: '是',
+  },
+  {
+    value: false,
+    label: '否',
+  },
+];
+
+export const btnConfigFormOptions = [
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'tagAttrs.value',
+        label: '按钮标题：',
+        required: true,
+      },
+      tagName: 'el-input',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'tagAttrs.value',
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'extraOption.relateFrom',
+        label: '选择表单：',
+        required: true,
+      },
+      tagName: 'el-select',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'extraOption.relateFrom',
+      extraOption: {
+        options: [
+          {
+            id: 1,
+            label: 's',
+          },
+        ],
+        props: {
+          key: 'id',
+          label: 'cnName',
+        },
+      },
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'extraOption.openType',
+        label: '打开方式：',
+        required: true,
+      },
+      tagName: 'el-select',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'extraOption.openType',
+      extraOption: {
+        options: openType,
+        props: {
+          key: 'id',
+          label: 'cnName',
+        },
+      },
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'extraOption.openUrl',
+        label: '跳转的url：',
+      },
+      tagName: 'el-input',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'extraOption.openUrl',
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'showType',
+        label: '显示形式：',
+      },
+      tagName: 'el-radio-group',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // TODO 没想好怎么做
+      formField: 'showType',
+      extraOption: {
+        options: showType,
+        props: {
+          key: 'value',
+          label: 'label',
+        },
+      },
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        label: '颜色',
+        required: true,
+      },
+      slotName: 'color',
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'tagAttrs.size',
+        label: '大小：',
+        required: true,
+      },
+      tagName: 'el-radio-group',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'tagAttrs.size',
+      extraOption: {
+        options: size,
+        props: {
+          key: 'value',
+          label: 'label',
+        },
+      },
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'tagAttrs.plain',
+        label: '朴素：',
+        required: true,
+      },
+      tagName: 'el-radio-group',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'tagAttrs.plain',
+      extraOption: {
+        options: yesOrNo,
+        props: {
+          key: 'value',
+          label: 'label',
+        },
+      },
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'tagAttrs.round',
+        label: '圆角：',
+        required: true,
+      },
+      tagName: 'el-radio-group',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'tagAttrs.round',
+      extraOption: {
+        options: yesOrNo,
+        props: {
+          key: 'value',
+          label: 'label',
+        },
+      },
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        prop: 'style',
+        label: '自定义样式：',
+        required: true,
+      },
+      tagName: 'el-input',
+      tagAttrs: {
+        placeholder: '',
+      },
+      // 对应formData中的属性值
+      formField: 'style',
+    },
+  },
+  {
+    elRowAttrs: {
+      gutter: 10,
+    },
+    formItem: {
+      formItemAttrs: {
+        label: '可执行函数：',
+        required: true,
+        prop: 'extraOption.fn',
+      },
+      tagName: 'el-input',
+      tagAttrs: {
+        placeholder: '',
+        type: 'textarea',
+      },
+      // 对应formData中的属性值
+      formField: 'extraOption.fn',
+    },
+  },
+];
+
+export function BtnConfigFrom() {
+  return {
+    style: '',
+    tagName: 'el-button',
+    tagAttrs: {
+      value: '',
+      type: '',
+      size: '',
+      plain: true,
+      round: false,
+    },
+    extraOption: {
+      relateFrom: '',
+      openType: 0,
+      openUrl: '',
+      fn: '',
+    },
+  };
+}
+
+export default {
+  BtnConfigFrom,
+  btnConfigFormOptions,
+  yesOrNo,
+  showType,
+  openType,
+  size,
+};
--- packages/setupBtnConfig/index.vue
+++ packages/setupBtnConfig/index.vue
@@ -1,250 +1,57 @@
 <template>
   <el-container>
-    <el-header v-if="showSearchFrom">
-      <base-render-form ref="form" :form-data="searchFrom" :form-options="formOptions" :use-dialog="false">
-      </base-render-form>
-    </el-header>
-
     <el-main>
-      <el-container style="height: 100%">
-        <el-header class="flex">
-          <base-render-regular ref="btnForm" :render-options="btnRegularOptions">
-          </base-render-regular>
-        </el-header>
-        <el-main>
-          <base-render-table ref="table" :table-data="tableData" :table-options="tableOptions"
-            @selection-change="selectListHandler">
-            <!-- 注意这里的slot值要和tableOptions中配置的slotName一致 -->
-            <!-- #operator是简写，详细请查阅vue文档 -->
-            <template #operator="{ row }">
-              <el-button v-if="row.$edit" @click="onSave(row)">
-                保存
-              </el-button>
-            </template>
-          </base-render-table>
-        </el-main>
-        <el-footer v-if="showPagination">
-          <el-pagination class="el-pagination" :layout="pageLayout" :total="page.totalCount"
-            :current-page.sync="page.pageNum" :page-size="page.pageSize"
-            @current-change="handleCurrentChange"></el-pagination>
-        </el-footer>
-      </el-container>
+      <base-render-form ref="form" :form-data="btnConfigFrom" :form-options="btnConfigFormOptions" @onSubmit="onSubmit"
+        label-position="top">
+        <template #color>
+          <el-row>
+            <span v-for="item in colorList" :key="item">
+              <el-button :type="item" @click="setBtnType(item)"></el-button> <el-button
+                v-if="btnConfigFrom.tagAttrs.type === item" type="success" icon="el-icon-check" circle></el-button>
+            </span>
+          </el-row>
+        </template>
+      </base-render-form>
     </el-main>
   </el-container>
 </template>
 
 <script>
-// 为何将pagination放在这个组件？首先因为baseTable仅仅是作为table的渲染器存在的，不应涉及网络请求，而pagination最重要的功能就是通过网络请求更新数据，因此在哪里使用到了获取table数据的网络请求，在哪就应该将pagination放进去
 
-import BaseRenderTable from '../BaseRenderTable/index';
+
 import BaseRenderForm from '../BaseRenderForm/index';
-import BaseRenderRegular from '../BaseRenderRegular/index';
-import { align, searchWidget } from '../../baseConfig/tableSelectConfigs';
-import { getElBtnConfig } from '../../baseConfig/widgetBaseConfig';
+import { btnConfigFormOptions, BtnConfigFrom } from "../../baseConfig/btnBaseConfig"
 import { setPlaceholder, getWidgetOptions, setColSpan } from '../../utils';
 import { cloneDeep, merge } from "lodash";
 
 export default {
   name: 'setupBtnConfig',
   components: {
-    BaseRenderTable,
     BaseRenderForm,
-    BaseRenderRegular
   },
   props: {
-    requestTableData: {
-      type: Function,
-      require: true
-    },
-    parseJson: {
-      type: Function,
-      require: true
-    },
-    requestTableConfig: {
-      type: Function,
-      require: true
-    },
-
-    pageLayout: {
-      type: Object,
-      default: function () {
-        ' ->, total,sizes, prev, pager, next,jumper'
-      }
-    }
   },
   data () {
     return {
-      // tabledata 属性值要做到和tableOptions中的prop相对应
-      tableConfigJSON: [],
-      tableOptions: [],
-      tableData: [],
-      formOptions: [],
-      searchFrom: {},
-      rawSearchFrom: {},
-      page: {
-        pageNum: 1,
-        pageSize: 10,
-        totalCount: 20
-      },
-      btnRegularOptions: [],
-      btnConfigJSON: [],
-      showSearchFrom: false,
-      // 初始化是否显示分页
-      showPagination: false,
+      colorList: ['', 'primary', 'success', 'info', 'warning', 'danger'],
+      btnConfigFormOptions,
+      btnConfigFromArr: [],
+      btnConfigFrom: new BtnConfigFrom(),
     };
   },
 
   mounted () {
-    this.init()
+    // this.init()
   },
-
   methods: {
-    // exec (fn) {
-    //   eval(fn);
-    // },
-
-    async init () {
-      this.queryTableData();
-      await this.queryTableConfig();
-      this.formOptions = this.composeFromOptions(this.tableConfigJSON);
-      this.tableOptions = this.tableConfigJSON.filter(item => item.show).map(item => {
-        const obj = {}
-        obj.prop = item.fieldCode
-        obj.label = item.fieldName
-        obj.align = align.find(alignitem => alignitem.id === item.align).value
-        obj['min-width'] = item.width
-        obj.sortable = !!item.sort
-        return obj
-      })
-      // this.exec("console.warn(this.formOptions)");
+    expose_getBtnConfigFromArr () {
+      return this.btnConfigFromArr
     },
-
-    // 由数据组成searchFrom
-    setFromField (source, field) {
-      this.$set(source, field, '');
+    onSubmit (data) {
+      this.btnConfigFromArr.push(data)
     },
-
-    // 设置searchFrom和装配fromOptions
-    composeFromOptions (tableData) {
-      this.showSearchFrom = false
-      if (!tableData.length) return [];
-      const { setFromField } = this;
-      const formOptions = [];
-      const length = tableData.length;
-      tableData.map((item, index) => {
-        const searchWidgetName = searchWidget.find((widgetitem) => widgetitem.id === item.searchWidget)?.tagName;
-        // 只有搜索控件有值，才会添加到options中
-        if (searchWidgetName) {
-          setFromField(this.searchFrom, item.fieldCode);
-          const options = getWidgetOptions(searchWidgetName, item)
-          formOptions.push(merge(options, item.searchWidgetConfig));
-        }
-        // 如果循环到最后一个且存在其他筛选项，则复制一份最原始的form
-        if (length - 1 === index && formOptions.length) {
-          this.rawSearchFrom = cloneDeep(this.searchFrom);
-          // 添加筛选和重置按钮
-          formOptions.push(...this.getBtnConfig());
-          this.showSearchFrom = true
-        }
-      });
-      return [{
-        elRowAttrs: {
-          gutter: 10,
-          type: "flex",
-          align: 'middle',
-          justify: 'start',
-        },
-        style: "flex-wrap: wrap",
-        formItem: formOptions
-      }];
-    },
-
-
-    getBtnConfig () {
-      const filterConfig = getElBtnConfig('primary', this.handleFilter, '筛选');
-      const resetConfig = getElBtnConfig('', this.handleReset, '重置');
-      setColSpan(filterConfig, 2);
-      setColSpan(resetConfig, 2);
-      return [filterConfig,
-        resetConfig];
-    },
-
-
-    handleFilter () {
-      this.queryTableData();
-    },
-    handleReset () {
-      this.searchFrom = cloneDeep(this.rawSearchFrom);
-      this.queryTableData();
-    },
-
-    onSave (row) {
-      // 推出编辑清空状态
-      row.$edit = false;
-      // this.$refs.table.expose_clearCurCellPro();
-    },
-
-    rowClick (val) {
-      this.$emit(rowClick, val);
-      console.log(val);
-    },
-
-    selectListHandler (val) {
-      this.$emit(selectListHandler, val);
-      console.log(val);
-    },
-
-
-    handleCurrentChange (val) {
-      console.log(val, this.page);
-      this.queryTableData();
-    },
-
-    queryTableData () {
-      const params = {
-        ...this.searchFrom,
-        ...this.page
-      };
-      return this.requestTableData(params).then(res => {
-        if (res.result === '0') {
-          this.tableData = res.data
-        } else {
-          console.error(`queryTableData message: ${res}`);
-        }
-      }).catch(e => {
-        console.error(`queryTableData error: ${e}`);
-      });
-    },
-
-    queryTableConfig () {
-      return this.requestTableConfig().then(res => {
-        if (res.result === '0') {
-          const { tableOptions, formOptions } = JSON.parse(res.data)
-          this.tableConfigJSON = tableOptions;
-          this.btnRegularOptions = this.composeBtnRegularOptions(formOptions.filter(item => item.isShow && item.isUse))
-        } else {
-          console.error(`queryTableConfig message: ${res}`);
-        }
-      }).catch(e => {
-        console.error(`queryTableConfig error: ${e}`);
-      });
-    },
-
-
-    composeBtnRegularOptions (config) {
-      const options = config.map(item => {
-        return getElBtnConfig('primary', this.handleFilter, item.btnName);
-      })
-      return [{
-        elRowAttrs: {
-          gutter: 10,
-          type: "flex",
-          align: 'middle',
-          justify: 'start',
-        },
-        style: "padding-left: 5px",
-        formItem: options
-      }]
+    setBtnType (type) {
+      this.btnConfigFrom.tagAttrs.type = type
     },
   }
 };
