1baad8a768e2776c7b9b115b2cdc31e444d4a2e5
张学强 <zhang.xueqiang>
2024-05-23 13:35:48 +0800

    fix: 右侧panel不可拖拽bug

--- packages/completeTable/component/panel.vue
+++ packages/completeTable/component/panel.vue
@@ -1,34 +1,35 @@
 <template>
   <div class="el-transfer-panel" @click.stop="panelClick">
     <div class="panel__body">
-      <el-input class="el-transfer-panel__filter" v-model="query" size="small" :placeholder="placeholder"
-        @mouseenter.native="inputHover = true" @mouseleave.native="inputHover = false">
+      <el-input
+        v-model="query"
+        class="el-transfer-panel__filter"
+        size="small"
+        :placeholder="placeholder"
+        @mouseenter.native="inputHover = true"
+        @mouseleave.native="inputHover = false"
+      >
         <i slot="prefix" :class="['el-input__icon', 'el-icon-' + inputIcon]" @click="clearQuery"></i>
       </el-input>
-      <el-checkbox-group v-model="checked" v-show="!hasNoMatch && data.length > 0"
-        :class="{ 'is-filterable': filterable }" class="el-transfer-panel__list">
-        <el-checkbox class="el-transfer-panel__item" :label="item[keyProp]" :key="item[keyProp]"
-          v-for="item in filteredData">
+      <el-checkbox-group v-show="!hasNoMatch && data.length > 0" v-model="checked" :class="{ 'is-filterable': filterable }" class="el-transfer-panel__list">
+        <el-checkbox v-for="item in filteredData" :key="item[keyProp]" class="el-transfer-panel__item" :label="item[keyProp]">
           <span>{{ item[labelProp] }}</span>
         </el-checkbox>
       </el-checkbox-group>
-      <p class="el-transfer-panel__empty" v-show="hasNoMatch">无匹配数据</p>
-      <p class="el-transfer-panel__empty" v-show="data.length === 0 && !hasNoMatch">无数据</p>
+      <p v-show="hasNoMatch" class="el-transfer-panel__empty">无匹配数据</p>
+      <p v-show="data.length === 0 && !hasNoMatch" class=" el-transfer-panel__empty">无数据</p>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'panel',
+  name: "Panel",
   props: {
     data: {
       type: Array,
-      default () {
-        return [
-
-        ];
+      default() {
+        return [];
       }
     },
     placeholder: String,
@@ -40,21 +41,20 @@ export default {
     props: Object
   },
 
-  data () {
+  data() {
     return {
       checked: [],
-      query: '',
+      query: "",
       inputHover: false,
-      labelProp: 'label',
-      keyProp: 'key',
+      labelProp: "label",
+      keyProp: "key"
     };
   },
 
-
   computed: {
-    filteredData () {
+    filteredData() {
       return this.data.filter(item => {
-        if (typeof this.filterMethod === 'function') {
+        if (typeof this.filterMethod === "function") {
           return this.filterMethod(this.query, item);
         } else {
           const label = item[this.labelProp] || item[this.keyProp].toString();
@@ -63,71 +63,70 @@ export default {
       });
     },
 
-    hasNoMatch () {
+    hasNoMatch() {
       return this.query.length > 0 && this.filteredData.length === 0;
     },
 
-    inputIcon () {
-      return this.query.length > 0 && this.inputHover
-        ? 'circle-close'
-        : 'search';
-    },
+    inputIcon() {
+      return this.query.length > 0 && this.inputHover ? "circle-close" : "search";
+    }
   },
 
   watch: {
-    checked (val) {
-      const arr = []
-      val = this.filteredData.map((item) => {
-        const key = this.keyProp
+    checked(val) {
+      const arr = [];
+      val = this.filteredData.map(item => {
+        const key = this.keyProp;
         if (val.includes(item[key])) {
-          arr.push(item[key])
+          arr.push(item[key]);
         }
-      })
-      this.$emit('checkedChange', arr);
+      });
+      this.$emit("checkedChange", arr);
     },
     data: {
       immediate: true,
-      handler (val) {
-        this.checked = val.map(item => item[this.keyProp])
+      handler(val) {
+        this.checked = val.map(item => item[this.keyProp]);
       }
     }
   },
 
-  inject: ['Sortable'],
+  inject: ["Sortable"],
 
-  mounted () {
-    this.rowDrop()
+  mounted() {
+    this.rowDrop();
   },
 
-
   methods: {
-    panelClick () { },
-    clearQuery () {
-      if (this.inputIcon === 'circle-close') {
-        this.query = '';
+    panelClick() {},
+    clearQuery() {
+      if (this.inputIcon === "circle-close") {
+        this.query = "";
       }
     },
 
-    rowDrop () {
+    rowDrop() {
       // 此时找到的元素是要拖拽元素的父容器
-      const dom = document.querySelector('.el-transfer-panel__list');
-      const key = this.keyProp
+      const dom = this.$el.querySelector(".el-transfer-panel__list");
+      const key = this.keyProp;
       this.Sortable.create(dom, {
         onEnd: e => {
-          //e.oldIndex为拖动一行原来的位置，e.newIndex为拖动后新的位置
+          // e.oldIndex为拖动一行原来的位置，e.newIndex为拖动后新的位置
           const targetRow = this.filteredData.splice(e.oldIndex, 1)[0];
           this.filteredData.splice(e.newIndex, 0, targetRow);
-          this.$emit('checkedChange', this.filteredData.map(item => item[key]));
+          this.$emit(
+            "checkedChange",
+            this.filteredData.map(item => item[key])
+          );
         }
-      })
-    },
+      });
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
 .panel__body {
-  height: 290px
+  height: 290px;
 }
 </style>
-
