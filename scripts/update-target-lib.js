560e753a57ebfadb8cd01c167f109f53952a8122
张学强 <zhang.xueqiang>
2024-08-02 10:27:29 +0800

    feat: 优化脚本

--- scripts/update-target-lib.js
+++ scripts/update-target-lib.js
@@ -1,23 +1,18 @@
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
-const targetDir =
-  process.argv[2] || '../../../prj/commondp-web/src/CommonDP/lib/tableDesign';
+const targetDir = process.argv[2] || "../../../prj/commondp-web/src/CommonDP/lib/tableDesign";
 
 const targetDirArr = [
-  '../../../prj/tablepro/baseConfig',
-  '../../../prj/tablepro/packages',
-  '../../../prj/tablepro/utils',
-  '../../../prj/tablepro/src',
+  "../../../prj/tablepro/baseConfig",
+  "../../../prj/tablepro/packages",
+  "../../../prj/tablepro/utils",
+  "../../../prj/tablepro/src",
+  "../../../prj/tablepro/mixins"
 ];
-const sourceDirArr = [
-  '../lowcode/baseConfig',
-  '../lowcode/packages',
-  '../lowcode/utils',
-  '../lowcode/src',
-];
+const sourceDirArr = ["../lowcode/baseConfig", "../lowcode/packages", "../lowcode/utils", "../lowcode/src", "../lowcode/mixins"];
 
-const sourceDir = process.argv[3] || './lib';
+const sourceDir = process.argv[3] || "./lib";
 
 const copy = (sd, td) => {
   // 读取目录下的文件，返回文件名及文件类型{name: 'xxx.txt, [Symbol(type)]: 1 }
@@ -29,14 +24,13 @@ const copy = (sd, td) => {
     const tagFile = path.resolve(td, file.name);
     // 文件是目录且未创建
     if (file.isDirectory() && !fs.existsSync(tagFile)) {
-      fs.mkdirSync(tagFile, (err) => console.log(err));
+      fs.mkdirSync(tagFile, err => console.log(err));
       copy(srcFile, tagFile);
     } else if (file.isDirectory() && fs.existsSync(tagFile)) {
       // 文件时目录且已存在
       copy(srcFile, tagFile);
     }
-    !file.isDirectory() &&
-      fs.copyFileSync(srcFile, tagFile, fs.constants.COPYFILE_FICLONE);
+    !file.isDirectory() && fs.copyFileSync(srcFile, tagFile, fs.constants.COPYFILE_FICLONE);
   }
 };
 
@@ -44,39 +38,39 @@ const run = async () => {
   const startTime = await new Date().getTime();
   console.log(!fs.existsSync(sourceDir));
   if (!fs.existsSync(sourceDir)) {
-    throw console.error('no such file or directory');
+    throw console.error("no such file or directory");
   } else if (!fs.existsSync(targetDir)) {
-    await fs.mkdirSync(targetDir, (err) => console.log(err));
+    await fs.mkdirSync(targetDir, err => console.log(err));
     await copy(sourceDir, targetDir);
   } else {
     await copy(sourceDir, targetDir);
   }
 
   const endTime = await new Date().getTime();
-  console.log('耗时:', ((endTime - startTime) / 1000).toFixed(2) + 's');
+  console.log("耗时:", ((endTime - startTime) / 1000).toFixed(2) + "s");
 };
 
 const run2 = async (sourceDir, targetDir) => {
   const startTime = await new Date().getTime();
   console.log(!fs.existsSync(sourceDir));
   if (!fs.existsSync(sourceDir)) {
-    throw console.error('run2 no such file or directory');
+    throw console.error("run2 no such file or directory");
   } else if (!fs.existsSync(targetDir)) {
-    await fs.mkdirSync(targetDir, (err) => console.log(err));
+    await fs.mkdirSync(targetDir, err => console.log(err));
     await copy(sourceDir, targetDir);
   } else {
     await copy(sourceDir, targetDir);
   }
 
   const endTime = await new Date().getTime();
-  console.log('run2耗时:', ((endTime - startTime) / 1000).toFixed(2) + 's');
+  console.log("run2耗时:", ((endTime - startTime) / 1000).toFixed(2) + "s");
 };
 run();
 targetDirArr.map((targetDir, index) => {
   const sourceDir = sourceDirArr[index];
   run2(sourceDir, targetDir);
 });
-const srcFile = path.resolve('../lowcode', 'package.json');
-const tagFile = path.resolve('../../../prj/tablepro', 'package.json');
+const srcFile = path.resolve("../lowcode", "package.json");
+const tagFile = path.resolve("../../../prj/tablepro", "package.json");
 console.log(srcFile, tagFile);
 fs.copyFileSync(srcFile, tagFile, fs.constants.COPYFILE_FICLONE);
